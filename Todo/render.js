39eab216ea2ddd8019e859e97f9681a209b63464
DanielRGilles <danryangilles@gmail.com>
2021-09-09 12:52:36 -0700

    everything seems to work

--- Todo/render.js
+++ Todo/render.js
@@ -1,21 +1,30 @@
-import { getTodos } from '../utils.js';
+import { completeToDos, getTodos } from '../utils.js';
 const toDos = document.getElementById('append-here');
 
 
-export function renderTodos(arrayItem) {
+export function renderTodo(arrayItem) {
     const li = document.createElement('li');
-    const liMsg = document.createElement('p');
+    const btn = document.createElement('button');
     
-    li.append(liMsg);
-    liMsg.classList.add('todos');
-    liMsg.textContent = arrayItem.todos; 
+    li.append(btn);
+    btn.classList.add('todos');
+    btn.setAttribute('id', arrayItem.id);
+    btn.textContent = arrayItem.todos;
+    
+    btn.addEventListener('click', () => {
+        completeToDos(arrayItem.id);
+        btn.style.textDecoration = 'line-through';
+    });
+    if (arrayItem.completed) {
+        btn.style.textDecoration = 'line-through';
+    }  
+    
     return li;
 }
 export function callRender() {
     const Todos = getTodos(); 
     for (let Todo of Todos) {
-        const liItem = renderTodos(Todo);
+        const liItem = renderTodo(Todo);
         toDos.append(liItem);
     }
-    
 }
--- Todo/todo-app.js
+++ Todo/todo-app.js
@@ -1,9 +1,18 @@
-import { addToDos } from '../utils.js';
+import { addToDos, getTodos, setTodos } from '../utils.js';
 import { callRender } from './render.js';
 const formEl = document.getElementById('add-items');
 const toDos = document.getElementById('append-here');
-
+const clearbtn = document.getElementById('clear-task');
 callRender();
+
+clearbtn.addEventListener('click', () => {
+    let doings = getTodos();
+    doings = [];
+    setTodos(doings);
+    toDos.innerHTML = '';
+    callRender();
+});
+
 
 formEl.addEventListener('submit', function(event) {
     event.preventDefault();
